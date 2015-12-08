162f82de49b717da266c5ae13cbec065431b3a7c
Mephala <gokhan.ozgozen@gmail.com>
2015-12-08 23:13:21 +0200

    Login credential checking ( Dummy ).

---
+++ src/main/java/com/gokhanabi/com/gokhanabi/ViewController.java
@@ -0,0 +1,25 @@
+package com.gokhanabi.com.gokhanabi;
+
+/**
+ * Created by Masraf2 on 12/8/2015.
+ */
+public class ViewController {
+
+    private static ViewController instance;
+
+    private ViewController(){}
+
+    public static synchronized ViewController getInstance(){
+        if (instance == null) {
+            instance = new ViewController();
+        }
+        return instance;
+    }
+
+
+    public boolean loginUser(String userName, String password){
+        return false;
+    }
+
+
+}
